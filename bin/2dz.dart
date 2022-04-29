a57ea14ef9e1b0c7496ceea8bddb1e926b9441cd
Samudinov <104208404+Samudinov@users.noreply.github.com>
2022-04-29 20:46:02 +0600

    first commit

---
+++ bin/2dz.dart
@@ -0,0 +1,15 @@
+void main(List<String> arguments) {
+  final String address = 'Suumbaeva st., Bishkek';
+  print(address.length);
+
+  int age = 20;
+  double weight = 63.2;
+  String name = 'Daniyar';
+
+  print('Возраст: $age, вес: $weight, имя: $name ');
+
+  int a = 20;
+  String b = '241292';
+  a = int.parse(b);
+  print(a);
+}
